2eae0be01e82a6cafb89b89f7f49fcd3031992df
Jonah <okirujonah@gmail.com>
2025-01-24 21:23:09 +0300

    Implement the display message to show the product is added to cart

--- beauty-ecommerce/src/App.jsx
+++ beauty-ecommerce/src/App.jsx
@@ -63,10 +63,18 @@ const Footer = ()=>{
 };
 
 function ProductCard({ product, addToCart, expandedProductId, setExpandedProductId }) {
+  const [successMessage, setSuccessMessage] = useState('');
   const isExpanded = expandedProductId === product.id;
   const toggleDetails = () => {
     setExpandedProductId(isExpanded ? null : product.id);
   }
+  const handleAddToCart = () => {
+    addToCart(product);
+    // set success message
+    setSuccessMessage(`Successfull added to cart!`);
+    // Clear the message
+    setTimeout(() => setSuccessMessage(''), 3000);
+  };
   return (
     <div className="border rounded p-4 flex flex-col items-center bg-pink-100 hover:shadow-2xl transition-shadow duration-300">
       <img src={product.image} alt={product.name} className="w-30 h-30 object-cover mb-4" />
@@ -83,10 +91,15 @@ function ProductCard({ product, addToCart, expandedProductId, setExpandedProduct
       )}
       <button
         className="mt-2 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-        onClick={() => addToCart(product)}
+        onClick={handleAddToCart}
       >
         Add to Cart
       </button>
+      {successMessage && (
+        <div className='text-center bg-green-100 text-green-800 p-2 mb-4 rounded'>
+          {successMessage}
+        </div>
+      )}
     </div>
   );
 }
@@ -133,6 +146,7 @@ function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [expandedProductId, setExpandedProductId] = useState(null);
   const [filteredProducts, setFilteredProducts] = useState(products);
+  
   
 
   const addToCart = (product) => {
@@ -187,10 +201,6 @@ function App() {
           </div>
         </nav>
         <h1 className='text-center font-extrabold pb-4 text-black'>PAMPERING BLISS</h1>
-
-        
-        
-
         <Routes>
           <Route
             path="/"
