e8141af2d9526be64120486398c38daf623cc8f4
Jonah <okirujonah@gmail.com>
2025-01-29 20:06:42 +0300

    update the on the package.json file

--- beauty-ecommerce/src/App.jsx
+++ beauty-ecommerce/src/App.jsx
@@ -58,7 +58,7 @@ function App() {
             />
             <button
               className="ml-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-              onClick={() => {}}
+              onClick={() => {handleSearch}}
             >
               Search
             </button>
